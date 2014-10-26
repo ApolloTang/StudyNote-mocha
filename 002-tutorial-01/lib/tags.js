cbfd33dd80905d5f0ba939f7b1821fa365fb6d47
Apollo Tang <tang.apollo@gmail.com>
2014-10-26 13:51:18 -0400

    added new test for checking default value of tags.parse

--- 002-tutorial-01/lib/tags.js
+++ 002-tutorial-01/lib/tags.js
@@ -7,19 +7,15 @@ exports.parse = function(args){
         // check for long formed tag
         // starting and including postion 0 for the length of 2
         if (arg.substr(0,2) === "--" ){
-            arg = arg.substr(2) // starting and including position 2 to the end
+            arg = arg.substr(2)                   // starting and including position 2 to the end
             if (arg.indexOf("=") !== -1 ) {
-                arg = arg.split("="); // break string at "=",
-                                      // and push fragments into array arg
-                var key = arg.shift(); // get the first one
-                var value = arg.join("=");  // second "=" is part of value
-
-                if ( /^[0-9]+$/.test(value) ) { // if value is type string
-                    value = parseInt(value, 10);
+                arg = arg.split("=");             // break string at "=", fragments hold in array "arg"
+                var key = arg.shift();            // get the first one
+                var value = arg.join("=");        // second "=" is part of value
+                if ( /^[0-9]+$/.test(value) ) {   // if value is type string and digits than
+                    value = parseInt(value, 10);  //      cast into integer of base 10
                 }
-
-                options[key] = value;  // second "=" is part of value
-
+                options[key] = value;             // store k-v pair in objected call options
             }
         }
     }
--- 002-tutorial-01/spec/tagsSpec.js
+++ 002-tutorial-01/spec/tagsSpec.js
@@ -3,11 +3,23 @@ var tags= require("../lib/tags.js");
 
 describe("Tags", function(){
     describe("#parse()", function(){
-       it("should parse long formed tags", function(){
-           var args = ["--depth=4", "--hello=world"];
-           var results = tags.parse(args);
-           expect(results).to.have.a.property("depth", 4);       // to have property "depth" w value 4
-           expect(results).to.have.a.property("hello", "world");
-       });
-   });
+        it("should parse long formed tags", function(){
+            var args = ["--depth=4", "--hello=world"];
+            var results = tags.parse(args);
+            expect(results).to.have.a.property("depth", 4);       // to have property "depth" w value 4
+            expect(results).to.have.a.property("hello", "world");
+        });
+        it("should fallback to defaults", function(){
+            var args = ["--depth=4", "--hello=world"];
+            var defaults = { depth:2, foo: "bar"};
+            var results = tags.parse(args, defaults);
+            var expexted = {
+                depth: 4
+                , foo: "bar"
+                , hello: "world"
+            };
+
+            expect(results).to.deep.equal(expected);  // use deep to match object
+        });
+    });
 });

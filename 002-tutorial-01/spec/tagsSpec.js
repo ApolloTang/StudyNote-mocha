b9c429a4b764478a171873955f21bf33650ad49c
Apollo Tang <tang.apollo@gmail.com>
2014-10-28 10:56:42 -0400

    added another test to test tag w/o value

--- 002-tutorial-01/spec/tagsSpec.js
+++ 002-tutorial-01/spec/tagsSpec.js
@@ -3,12 +3,14 @@ var tags= require("../lib/tags.js");
 
 describe("Tags", function(){
     describe("#parse()", function(){
+
         it("should parse long formed tags", function(){
             var args = ["--depth=4", "--hello=world"];
             var results = tags.parse(args);
             expect(results).to.have.a.property("depth", 4);       // to have property "depth" w value 4
             expect(results).to.have.a.property("hello", "world");
         });
+
         it("should fallback to defaults", function(){
             var args = ["--depth=4", "--hello=world"];
             var defaults = { depth:2, foo: "bar"};
@@ -19,6 +21,13 @@ describe("Tags", function(){
                 , hello: "world"
             };
             expect(results).to.deep.equal(expected);  // use deep to match object
+        });
+
+        it("should accept tags without values as a bool", function(){
+            var args = ["--searchContents"];
+            var results = tags.parse(args);
+
+            expect(results).to.have.a.property("searchContents");
         });
     });
 });

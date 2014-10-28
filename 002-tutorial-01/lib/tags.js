14301e20f64d30aabdf1368d7a59ab72d9740719
Apollo Tang <tang.apollo@gmail.com>
2014-10-28 11:19:45 -0400

    added codd to pass test; edit comments

--- 002-tutorial-01/lib/tags.js
+++ 002-tutorial-01/lib/tags.js
@@ -9,18 +9,20 @@ exports.parse = function(args, defaults){
 
     for (var i in args){
         var arg = args[i];
-        // check for long formed tag
-        // starting and including postion 0 for the length of 2
-        if (arg.substr(0,2) === "--" ){
-            arg = arg.substr(2)                   // starting and including position 2 to the end
-            if (arg.indexOf("=") !== -1 ) {
-                arg = arg.split("=");             // break string at "=", fragments hold in array "arg"
-                var key = arg.shift();            // get the first one
-                var value = arg.join("=");        // second "=" is part of value
+        if (arg.substr(0,2) === "--" ){           // check to see the first 2 chars is "--"  this is long formed tag
+            arg = arg.substr(2)                   // if it is long form tag save everything after "--" in arg
+            if (arg.indexOf("=") !== -1 ) {       // if one or more delimiter ("=") exsits
+                arg = arg.split("=");             //    break string into fracgments at "=", fragments hold in array "arg"
+                var key = arg.shift();            // get the first one, which is the key, fracgemts left in arg is now value(s)
+                var value = arg.join("=");        // second "=" is part of value, so concatenate all fragment into 1 piece
                 if ( /^[0-9]+$/.test(value) ) {   // if value is type string and digits than
                     value = parseInt(value, 10);  //      cast into integer of base 10
                 }
                 options[key] = value;             // store k-v pair in objected call options
+            } else {
+                options[arg] = true;              // if there is no "=", then tag only has key and no value
+                                                  // it is then default to boolean "true"
+                console.log(key , options[key])
             }
         }
     }

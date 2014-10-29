2ede10c944bee262b2ce250418a32df1a4cca494
Apollo Tang <tang.apollo@gmail.com>
2014-10-29 17:17:26 -0400

    edit comment

--- 002-tutorial-01/lib/tags.js
+++ 002-tutorial-01/lib/tags.js
@@ -9,20 +9,19 @@ exports.parse = function(args, defaults){
 
     for (var i in args){
         var arg = args[i];
-        if (arg.substr(0,2) === "--" ){           // check to see the first 2 chars is "--"  this is long formed tag
-            arg = arg.substr(2)                   // if it is long form tag save everything after "--" in arg
-            if (arg.indexOf("=") !== -1 ) {       // if one or more delimiter ("=") exsits
+        if (arg.substr(0,2) === "--" ){           // check to see the first 2 chars is "--", if so this is long formed tag
+            arg = arg.substr(2)                   // if it is long form tag save chas after "--" in arg
+            if (arg.indexOf("=") !== -1 ) {       // if one or more delimiter ("=") exsits then
                 arg = arg.split("=");             //    break string into fracgments at "=", fragments hold in array "arg"
-                var key = arg.shift();            // get the first one, which is the key, fracgemts left in arg is now value(s)
+                var key = arg.shift();            // get the first one, which is the key, fragments left in arg is the value
                 var value = arg.join("=");        // second "=" is part of value, so concatenate all fragment into 1 piece
-                if ( /^[0-9]+$/.test(value) ) {   // if value is type string and digits than
+                if ( /^[0-9]+$/.test(value) ) {   // if value is type string and one or more digits only than
                     value = parseInt(value, 10);  //      cast into integer of base 10
                 }
                 options[key] = value;             // store k-v pair in objected call options
             } else {
-                options[arg] = true;              // if there is no "=", then tag only has key and no value
+                options[arg] = true;              // if there is no "=", then tag only has key and consist of no value
                                                   // it is then default to boolean "true"
-                console.log(key , options[key])
             }
         }
     }
